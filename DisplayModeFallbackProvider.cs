fcf3197898b0cec742b385f272ab54f378716184
Mark Everard <markeverard@hotmail.com>
2014-11-27 18:29:50 +0000

    Added feature to allow me to override the display options and css classes registered, allowing an implementor to provide their own display options / bootstrap classes to use. Modified the way that display options are synchronised with the DDS
    Need to look at unit tests

---
+++ DisplayModeFallbackProvider.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace EPiBootstrapArea
+{
+    public class DisplayModeFallbackProvider : IDisplayModeFallbackProvider
+    {
+        public List<DisplayModeFallback> GetAll()
+        {
+            var initialData = new List<DisplayModeFallback>
+            {
+                new DisplayModeFallback
+                {
+                    Name = "Full width (1/1)",
+                    Tag = ContentAreaTags.FullWidth,
+                    LargeScreenWidth = 12,
+                    MediumScreenWidth = 12,
+                    SmallScreenWidth = 12,
+                    ExtraSmallScreenWidth = 12,
+                    Icon = "epi-icon__layout--full"
+                },
+                new DisplayModeFallback
+                {
+                    Name = "Half width (1/2)",
+                    Tag = ContentAreaTags.HalfWidth,
+                    LargeScreenWidth = 6,
+                    MediumScreenWidth = 6,
+                    SmallScreenWidth = 12,
+                    ExtraSmallScreenWidth = 12,
+                    Icon = "epi-icon__layout--half"
+                },
+                new DisplayModeFallback
+                {
+                    Name = "One third width (1/3)",
+                    Tag = ContentAreaTags.OneThirdWidth,
+                    LargeScreenWidth = 4,
+                    MediumScreenWidth = 6,
+                    SmallScreenWidth = 12,
+                    ExtraSmallScreenWidth = 12,
+                    Icon = "epi-icon__layout--one-third"
+                },
+                new DisplayModeFallback
+                {
+                    Name = "Two thirds width (2/3)",
+                    Tag = ContentAreaTags.TwoThirdsWidth,
+                    LargeScreenWidth = 8,
+                    MediumScreenWidth = 6,
+                    SmallScreenWidth = 12,
+                    ExtraSmallScreenWidth = 12,
+                    Icon = "epi-icon__layout--two-thirds"
+                },
+                new DisplayModeFallback
+                {
+                    Name = "One quarter width (1/4)",
+                    Tag = ContentAreaTags.OneQuarterWidth,
+                    LargeScreenWidth = 3,
+                    MediumScreenWidth = 6,
+                    SmallScreenWidth = 12,
+                    ExtraSmallScreenWidth = 12,
+                    Icon = "epi-icon__layout--one-quarter"
+                },
+                new DisplayModeFallback
+                {
+                    Name = "Three quarters width (3/4)",
+                    Tag = ContentAreaTags.ThreeQuartersWidth,
+                    LargeScreenWidth = 9,
+                    MediumScreenWidth = 6,
+                    SmallScreenWidth = 12,
+                    ExtraSmallScreenWidth = 12,
+                    Icon = "epi-icon__layout--three-quarters"
+                },
+            };
+            return initialData;
+        }
+    }
+}
