42bc5f94122f18ede72baa14dd67a2e66d0c2c2a
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2015-09-05 12:10:22 +0300

    Added callback to modify start tag element for the block

--- BootstrapAwareContentAreaRenderer.cs
+++ BootstrapAwareContentAreaRenderer.cs
@@ -15,6 +15,12 @@ namespace EPiBootstrapArea
     {
         private static bool _fallbackCached;
         private static IEnumerable<DisplayModeFallback> _fallbacks;
+        private Action<HtmlNode, ContentAreaItem, IContent> _elementStartTagRenderCallback;
+
+        protected void SetElementStartTagRenderCallback(Action<HtmlNode, ContentAreaItem, IContent> callback)
+        {
+            _elementStartTagRenderCallback = callback;
+        }
 
         protected override string GetContentAreaItemCssClass(HtmlHelper htmlHelper, ContentAreaItem contentAreaItem)
         {
@@ -36,19 +42,22 @@ namespace EPiBootstrapArea
             string cssClass)
         {
             var originalWriter = htmlHelper.ViewContext.Writer;
-            var tempWriter = new StringWriter();
 
+            var tempWriter = new StringWriter();
             htmlHelper.ViewContext.Writer = tempWriter;
             var content = contentAreaItem.GetContent(ContentRepository);
 
             try
             {
                 base.RenderContentAreaItem(htmlHelper, contentAreaItem, templateTag, htmlTag, cssClass);
-
                 var contentItemContent = tempWriter.ToString();
-                var shouldRender = IsInEditMode(htmlHelper);
 
-                if (!shouldRender)
+                if (IsInEditMode(htmlHelper))
+                {
+                    // we need to render block if we are in Edit mode
+                    originalWriter.Write(contentItemContent);
+                }
+                else
                 {
                     var doc = new HtmlDocument();
                     doc.Load(new StringReader(contentItemContent));
@@ -56,19 +65,30 @@ namespace EPiBootstrapArea
 
                     if (blockContentNode != null)
                     {
-                        shouldRender = !string.IsNullOrEmpty(blockContentNode.InnerHtml);
-                        if (!shouldRender)
+                        // pass node to callback for some fancy modifications (if any)
+                        if (_elementStartTagRenderCallback != null)
+                        {
+                            _elementStartTagRenderCallback(blockContentNode, contentAreaItem, content);
+                        }
+
+                        var writer = new StringWriter();
+                        doc.Save(writer);
+                        contentItemContent = writer.ToString();
+
+                        if (!string.IsNullOrEmpty(blockContentNode.InnerHtml))
+                        {
+                            originalWriter.Write(contentItemContent);
+                        }
+                        else
                         {
                             // ReSharper disable once SuspiciousTypeConversion.Global
                             var visibilityControlledContent = content as IControlVisibility;
-                            shouldRender = (visibilityControlledContent == null) || (!visibilityControlledContent.HideIfEmpty);
+                            if ((visibilityControlledContent == null) || (!visibilityControlledContent.HideIfEmpty))
+                            {
+                                originalWriter.Write(contentItemContent);
+                            }
                         }
                     }
-                }
-
-                if (shouldRender)
-                {
-                    originalWriter.Write(contentItemContent);
                 }
             }
             finally
@@ -103,7 +123,7 @@ namespace EPiBootstrapArea
         private static string GetTypeSpecificCssClasses(ContentAreaItem contentAreaItem, IContentRepository contentRepository)
         {
             var content = contentAreaItem.GetContent(contentRepository);
-            var cssClass = content == null ? String.Empty : content.GetOriginalType().Name.ToLowerInvariant();
+            var cssClass = content == null ? string.Empty : content.GetOriginalType().Name.ToLowerInvariant();
 
             // ReSharper disable once SuspiciousTypeConversion.Global
             var customClassContent = content as ICustomCssInContentArea;
