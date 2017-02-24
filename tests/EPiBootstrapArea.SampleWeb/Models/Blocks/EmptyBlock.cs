6f65449c6b0ea868d7f4e0c08adbbcf69527058d
valdisiljuconoks <valdis@VALDIS-PC>
2017-02-24 13:11:24 +0200

    Test case for empty blocks

---
+++ tests/EPiBootstrapArea.SampleWeb/Models/Blocks/EmptyBlock.cs
@@ -0,0 +1,11 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace EPiBootstrapArea.SampleWeb.Models.Blocks
+{
+    [ContentType(DisplayName = "EmptyBlock", GUID = "0e641414-823c-486d-92c3-46db04c10e4a", Description = "")]
+    public class EmptyBlock : BlockData, IControlVisibility
+    {
+        public bool HideIfEmpty => true;
+    }
+}
