89261c06022bb4a0e4c781e03ddbfc0076b46ad0
valdisiljuconoks <valdis@VALDIS-PC>
2017-06-16 23:23:59 +0300

    Version adjustment

--- build-packages.ps1
+++ build-packages.ps1
@@ -1,5 +1,5 @@
 cd .\.nuget
 
 .\nuget.exe pack ..\src\EPiBootstrapArea\EPiBootstrapArea.csproj -Properties Configuration=Release
-.\nuget.exe pack ..\src\EPiBootstrapArea.Forms\EPiBootstrapArea.Forms.csproj -Properties Configuration=Release
+.\nuget.exe pack ..\src\EPiBootstrapArea.Forms\EPiBootstrapArea.Forms.csproj -Properties Configuration=Release -IncludeReferencedProjects
 cd ..\
