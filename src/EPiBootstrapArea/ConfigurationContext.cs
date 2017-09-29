a1852a68b1b4915e424f5b3a5f25c7e39ff11e65
valdisiljuconoks <valdis@VALDIS-PC>
2017-09-30 00:28:35 +0300

    * Added config parameter to disable built-in display options registration

---
+++ src/EPiBootstrapArea/ConfigurationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EPiBootstrapArea
+{
+    public class ConfigurationContext
+    {
+        public bool RowSupportEnabled { get; set; }
+
+        public bool AutoAddRow { get; set; }
+
+        public bool DisableBuiltinDisplayOptions { get; set; }
+
+        public static ConfigurationContext Current { get; } = new ConfigurationContext();
+
+        public static void Setup(Action<ConfigurationContext> configCallback)
+        {
+            configCallback?.Invoke(Current);
+        }
+    }
+}
