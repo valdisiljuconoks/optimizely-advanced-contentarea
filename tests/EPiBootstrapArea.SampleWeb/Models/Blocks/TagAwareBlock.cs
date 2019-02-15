42c537021d63dfaac70e0c22b189e369955f82f3
Eric Maddox <eric.maddox@episerver.com>
2019-02-15 17:07:28 -0500

    Test code used to show Multi-threaded problem with tags. #37

---
+++ tests/EPiBootstrapArea.SampleWeb/Models/Blocks/TagAwareBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EPiBootstrapArea.SampleWeb.Models.Blocks
+{
+    [ContentType(DisplayName = "TagAwareBlock", GUID = "4187abf5-e4f4-4d9b-817e-a97229080b61", Description = "")]
+    public class TagAwareBlock : BlockData
+    {
+
+    }
+}
